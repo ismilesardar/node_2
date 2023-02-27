84ea972ec60d1d7f0f61b8a9143d321074120484
ismail <ismailsardar@gmail.com>
2023-02-27 10:50:47 +0600

    Module Wrapper

--- moduleSchope/iife.js
+++ moduleSchope/iife.js
@@ -7,3 +7,9 @@
     let superHero = "SuperMan"
     console.log(superHero);
 })();
+
+//node.js module tapper
+(function(exports, require, module, __filename, __dirname){
+    let superHero = "IronMan";
+    console.log(superHero);
+})();
