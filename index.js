646a0c5a2f6ba7f6abe0f7dee0bbd54c36c6fd42
ismail <ismailsardar@gmail.com>
2023-02-27 10:23:15 +0600

    Module Scope and IIFE

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const sum2 = add(3,3);
 console.log(sum)
 console.log(sum2)
 
+console.clear()
+require('./moduleSchope/batman')
+require('./moduleSchope/superMan')
+
---
+++ moduleSchope/iife.js
@@ -0,0 +1,9 @@
+(function () {
+    let superHero = "BatMan"
+    console.log(superHero);
+})();
+
+(function () {
+    let superHero = "SuperMan"
+    console.log(superHero);
+})();
